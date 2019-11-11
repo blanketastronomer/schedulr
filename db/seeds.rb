0000cb533fdf570c9d79759f7c18f565ebdbd0e1
Nicholas S <blanketastronomer@users.noreply.github.com>
2019-11-11 05:46:56 -0800

    Allow first user's password to be set from ENV
    The first seeded user needs to have a user-defined password, but we
    don't want to have it set in version control, since you never know if an
    end-user will reset the password or not.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ORIGINAL_ADMIN_PASSWORD = 'ORIGINAL_ADMIN_PASSWORD'
+original_admin_password = ENV[ORIGINAL_ADMIN_PASSWORD]
+
+class EnvironmentVariableMissingError < StandardError
+  def initialize(environment_variable)
+    @environment_variable = environment_variable
+  end
+
+  def message
+    "Environment variable #{@environment_variable} is missing!"
+  end
+end
+
+class EnvironmentVariableBlankError < StandardError
+  def initialize(environment_variable)
+    @environment_variable = environment_variable
+  end
+
+  def message
+    "Environment variable #{@environment_variable} is blank!"
+  end
+end
+
+case
+when original_admin_password.nil?
+  raise EnvironmentVariableMissingError.new(ORIGINAL_ADMIN_PASSWORD)
+when original_admin_password.blank?
+  raise EnvironmentVariableBlankError.new(ORIGINAL_ADMIN_PASSWORD)
+else
+  User.create(email: 'admin@example.com', password: ENV[ORIGINAL_ADMIN_PASSWORD])
+end
